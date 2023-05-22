5875e60266e26bec70762ae063c5787f61505554
Sensol2 <hhncn4471@soongsil.ac.kr>
2023-05-22 20:22:28 +0900

    fix: d.ts 파일 추가

--- src/navigation/screens/Analysis/VoicePitchDetector.tsx
+++ src/navigation/screens/Analysis/VoicePitchDetector.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, Button} from 'react-native';
+import {View, StyleSheet, Button, Text} from 'react-native';
 import {PitchDetector} from 'react-native-pitch-detector';
 
 import {AnalysisStackParamList} from '../../../types/stacks/AnalysisStackTypes';
@@ -11,7 +11,19 @@ export type VoicePitchDetectorProps = StackScreenProps<
 
 export default function VoicePitchDetector() {
   const [recording, setRecording] = React.useState<boolean>();
-  const subscription = PitchDetector.addListener(console.log);
+  const [pitch, setPitch] = React.useState<number | null>(null);
+
+  React.useEffect(() => {
+    const subscription = PitchDetector.addListener(data => {
+      console.log('음역:', data.tone);
+      setPitch(data.tone);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   async function startRecording() {
     console.log('Start recording');
     setRecording(true);
@@ -30,6 +42,7 @@ export default function VoicePitchDetector() {
         title={recording ? '녹음 중지' : '녹음하기'}
         onPress={recording ? stopRecording : startRecording}
       />
+      {pitch && <Text style={styles.pitchText}>Detected Pitch: {pitch}</Text>}
     </View>
   );
 }
@@ -48,5 +61,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     color: 'white',
     borderRadius: 10,
+  },
+  pitchText: {
+    marginTop: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
