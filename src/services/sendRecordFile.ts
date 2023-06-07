97c3de4578fb16efe44cf55685ce3ea5405c3335
Hayeon-Lee <papepopepe@naver.com>
2023-06-08 01:02:41 +0900

    feat: userService class define

--- src/services/sendRecordFile.ts
+++ src/services/sendRecordFile.ts
@@ -1,14 +1,14 @@
-import axios from "axios";
-import { SERVER_URL } from "../data/serverData";
-import * as FileSystem from "expo-file-system";
-import { Buffer } from "buffer";
+import axios from 'axios';
+import {SERVER_URL} from '../data/serverData';
+import * as FileSystem from 'expo-file-system';
+import {Buffer} from 'buffer';
 
 class sendRecordFile {
   //S3 링크 받아오기
   async GetS3URL(filename: string) {
     try {
       const response = await axios.get(
-        `${SERVER_URL}/recording/upload-url?filename=${filename}`
+        `${SERVER_URL}/recording/upload-url?filename=${filename}`,
       );
       return response;
     } catch (error) {
@@ -18,11 +18,11 @@ class sendRecordFile {
 
   //S3에 파일 전송하기
   async SendFileS3(s3url: string, file: string) {
-    const buffer = Buffer.from(file, "base64"); //base64로 바꾼 파일을 buffer 로 저장
+    const buffer = Buffer.from(file, 'base64'); //base64로 바꾼 파일을 buffer 로 저장
     return fetch(s3url, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        "Content-Type": "audio/wav; charset=utf-8",
+        'Content-Type': 'audio/wav; charset=utf-8',
       },
       body: buffer,
     });
